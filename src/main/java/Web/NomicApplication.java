8eeb4f4fde267b14c6e07e7e22a204d3702314db
Stuart Holland <soulc3@gmail.com>
2013-05-25 20:14:56 +0100

    Initial commit - basic JWT working.

---
+++ src/main/java/Web/NomicApplication.java
@@ -0,0 +1,12 @@
+package Web;
+
+import eu.webtoolkit.jwt.WApplication;
+import eu.webtoolkit.jwt.WEnvironment;
+
+public class NomicApplication extends WApplication {
+
+	public NomicApplication(WEnvironment env) {
+		super(env);
+	}
+
+}
